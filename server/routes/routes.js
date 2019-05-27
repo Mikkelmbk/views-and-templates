1584a6b6a83952c98d08559f539dd3ce77cd694a
Mikkel Back Koll <217236@RTS-365.dk>
2019-05-27 10:53:19 +0200

    titles partial fil lavet, forEach lavet inde i produkt siden, den bliver kørt på arrayet i routes.js filen

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,16 +1,32 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home', {
+         "title":"Forside"
+      });
    });
 
 
    app.get('/product', (req, res, next) => {
-      res.render('product');
+      res.render('product', {
+         "title":"Produkt",
+         "products":[
+            {
+               "name":"produkt1",
+               "price":400
+            },
+            {
+               "name":"produkt2",
+               "price":700
+            }
+         ]
+      });
    });
 
 
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact', {
+         "title":"Kontakt"
+      });
    });
 
 
